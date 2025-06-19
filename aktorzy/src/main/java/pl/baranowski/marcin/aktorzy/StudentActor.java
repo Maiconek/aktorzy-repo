499d6414a393f196e7d55405e849a89a3df44bd7
Marcin Baranowski <mbaranowski57@gmail.com>
2025-06-19 21:20:17 +0200

    test innego podejscia

--- aktorzy/src/main/java/pl/baranowski/marcin/aktorzy/StudentActor.java
+++ aktorzy/src/main/java/pl/baranowski/marcin/aktorzy/StudentActor.java
@@ -1,9 +1,12 @@
 package pl.baranowski.marcin.aktorzy;
 import akka.actor.AbstractActor;
+import akka.actor.ActorRef;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestClient;
+
+import java.util.concurrent.CompletableFuture;
 
 public class StudentActor extends AbstractActor {
     private static final Logger log = LoggerFactory.getLogger(StudentActor.class);
@@ -14,19 +17,39 @@ public class StudentActor extends AbstractActor {
         this.restClient = restClient;
     }
 
+//    @Override
+//    public Receive createReceive() {
+//        return receiveBuilder()
+//                .match(Integer.class, seconds -> {
+//                    // Przetwarzanie żądania
+//                    log.info("Processing block request for {} seconds", seconds);
+//                    ResponseEntity<Void> result = restClient.get()
+//                            .uri("/students/block/" + seconds)
+//                            .retrieve()
+//                            .toBodilessEntity();
+//
+//                    // Odpowiedź
+//                    getSender().tell(result.getStatusCode().toString(), getSelf());
+//                })
+//                .build();
+//    }
+
     @Override
     public Receive createReceive() {
         return receiveBuilder()
                 .match(Integer.class, seconds -> {
-                    // Przetwarzanie żądania
-                    log.info("Processing block request for {} seconds", seconds);
-                    ResponseEntity<Void> result = restClient.get()
-                            .uri("/students/block/" + seconds)
-                            .retrieve()
-                            .toBodilessEntity();
+                    ActorRef senderRef = getSender();  // kopia bo później może się zmienić
 
-                    // Odpowiedź
-                    getSender().tell(result.getStatusCode().toString(), getSelf());
+                    CompletableFuture
+                            .supplyAsync(() -> {
+                                log.info("Processing block request for {} seconds", seconds);
+                                ResponseEntity<Void> result = restClient.get()
+                                        .uri("/students/block/" + seconds)
+                                        .retrieve()
+                                        .toBodilessEntity();
+                                return result.getStatusCode().toString();
+                            })
+                            .thenAccept(response -> senderRef.tell(response, getSelf()));
                 })
                 .build();
     }
