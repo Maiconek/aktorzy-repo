b43d8afb77245d9e53e4aa8eebfa02f3399c3e11
Marcin Baranowski <mbaranowski57@gmail.com>
2025-06-30 20:33:38 +0200

    changed pool to 20

--- aktorzy/src/main/java/pl/baranowski/marcin/aktorzy/AkkaConfig.java
+++ aktorzy/src/main/java/pl/baranowski/marcin/aktorzy/AkkaConfig.java
@@ -19,7 +19,7 @@ public class AkkaConfig {
     @Bean
     public ActorRef studentActor(ActorSystem actorSystem, RestClient restClient) {
         return actorSystem.actorOf(
-                new RoundRobinPool(10).props(Props.create(StudentActor.class, restClient)),
+                new RoundRobinPool(20).props(Props.create(StudentActor.class, restClient)),
                 "studentActorRouter"
         );
     }
